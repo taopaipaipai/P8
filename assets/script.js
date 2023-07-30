1774847f52c76b3322135fa9ab4b31052c76c100
taopaipaipai <tao.paipai9380@gmail.com>
2023-07-30 08:11:26 +0200

    Étape 2 : Ajoutez des Event Listeners sur les flèches (point 1 et 2 Ok)

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,37 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+// 30/07/2023 06:30:33 evenement si clique affiche le message "Salut"
+element.addEventListener("click", cliqueGauche);
+element.addEventListener("click", cliqueDroit);
+
+function cliqueGauche() {
+	// avec la metode alerte
+  alert ("vous avez cliqué sur le Arrow de Gauche");
+}
+
+function cliqueDroit() {
+	// avec la metode alerte
+  alert ("vous avez cliqué sur le Arrow de Droit");
+}
+
+// plus complexe : surevillance de la souris et dit sur quelle bouton on a clické !!
+
+let button = document.querySelector("arrow_right");
+button.addEventListener("mouseup", (e) => {
+  let log = document.querySelector("#log");
+  switch (e.button) {
+    case 0:
+      log.textContent = "Left button clicked.";
+      break;
+    case 1:
+      log.textContent = "Middle button clicked.";
+      break;
+    case 2:
+      log.textContent = "Right button clicked.";
+      break;
+    default:
+      log.textContent = `Unknown button code: ${e.button}`;
+  }
+});
