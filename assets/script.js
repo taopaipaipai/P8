ae86d33116593b9e386b8a1b9ef82af904a08156
taopaipaipai <tao.paipai9380@gmail.com>
2023-09-13 08:06:11 +0200

    Étape 4 : Modifiez le slide au clic sur le bouton

--- assets/script.js
+++ assets/script.js
@@ -25,14 +25,6 @@ const ArrowDroit  = document.querySelector(".arrow_right"); // suite cours de fr
 const ArrowGauche = document.querySelector("#banner .arrow_left"); // suite cours de from Scrtach il fallait mettre le . pour que la class soit reconnue !! 10/08/2023 17:50:50
 const ArrowDroit  = document.querySelector("#banner .arrow_right"); // suite cours de from Scrtach il fallait mettre le . pour que la class soit reconnue !! 10/08/2023 17:50:50
 
-// maj et correction le 10/08/2023 18:18:45
-ArrowGauche.addEventListener("click", ()=> {
-	console.log ("vous avez cliqué sur le Arrow de Gauche");
-	});
-
-ArrowDroit.addEventListener('click', ()=> {
-	console.log ("vous avez cliqué sur le Arrow de Droit");
-	});
 
 // 14/08/2023 13:01:03 --> taille d'un tableau
 let tailleTableau = slides.length
@@ -45,13 +37,32 @@ const Dots = document.querySelector(".dots")
 for (let i=0; i<tailleTableau; i++) {
 	 const i = document.createElement("div")
 	 i.classList.add("dot")
-	 Dots.appendChild(i) 
+	 Dots.appendChild(i)
 }
 
 console.log(Dots)
 
+// reprise le 09/09/2023 13:44:56
+const bannerImage = document.querySelector(".banner-img");
+const bannerText = document.querySelector("#banner p");
+
 // initialisation du compteur
 let currentIndex = 0;
+
+// fonction pour passer au slide suivant ou précédent avec pour entrée un nombre (-1) ou (+1)
+
+function majSlider(NavigueSlide) {
+ 
+ currentIndex = currentIndex + NavigueSlide;
+ 
+ const slide = slides[currentIndex];
+ console.log(currentIndex);
+ console.log(slide.image);
+ console.log(slide.tagLine);
+ bannerImage.src = `./assets/images/slideshow/${slide.image}`;  // j'ai reussi à afficher une autre images le 09/09/2023 à 14:09:47 !!
+ bannerText.innerHTML = slide.tagLine;
+
+}
 
 // 11/09/2023 13:03:07
 // Fonction pour mettre à jour le style des bullet points
@@ -67,3 +78,17 @@ function majBulletActive() {
 }
 // met en place le bullet actif pour la 1er fois au chargement de la page
 majBulletActive();
+
+// maj et correction le 10/08/2023 18:18:45, puis revisiter le 09/09/2023 pour ajouter les fonctions majSlider() et majBulletActive()
+ArrowGauche.addEventListener("click", ()=> {
+	console.log ("vous avez cliqué sur le Arrow de Gauche");
+	majSlider(-1);
+	majBulletActive();
+	});
+
+ArrowDroit.addEventListener('click', ()=> {
+	console.log ("vous avez cliqué sur le Arrow de Droit");
+	majSlider(1);
+	majBulletActive();
+	});
+
